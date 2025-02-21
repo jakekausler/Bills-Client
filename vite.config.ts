6ee272efbb2b6270b93bc2eb8355b0b820db9c68
jakekausler <jakekausler@yahoo.com>
2025-02-21 16:31:57 +0000

    Add ports to env

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5002",
+      '/api': {
+        target: `http://localhost:${process.env.SERVER_PORT}`,
         changeOrigin: true,
       },
     },
